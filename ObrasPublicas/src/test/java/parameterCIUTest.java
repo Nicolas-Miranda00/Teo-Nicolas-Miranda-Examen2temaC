722ed1aa517c5e7904db5218032f9ad9c827a608
Nico-ceo <teito.bris@gmail.com>
2026-06-13 19:34:27 -0300

    actualizaciones

--- ObrasPublicas/src/test/java/parameterCIUTest.java
+++ ObrasPublicas/src/test/java/parameterCIUTest.java
@@ -15,31 +15,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class parameterCIUTest {
 
-      public static Stream<Arguments> tomarDatos() {
+    public static Stream<Arguments> tomarDatos() {
 
-        return Stream.of(
-                Arguments.of("juan@gmail.com", true),
-                Arguments.of("ana@yahoo.com", true),
+        return Stream.of(Arguments.of("jorgesys@tototita.com", true),
+                Arguments.of("andre@ulp.com", true),
                 Arguments.of("correo", false),
                 Arguments.of("pepe@", false)
+                
         );
     }
 
     @ParameterizedTest
     @MethodSource("tomarDatos")
-    public void testValidarEmail(String email,
-                                 boolean esperado) {
-
-        assertEquals(
-                esperado,
-                validarMail(email));
+    public void testValidarEmail(String email, boolean esperado) {       
+        assertEquals( esperado, validarMail(email));                         
     }
 
     public static boolean validarMail(String email) {
 
-        Pattern pattern = Pattern.compile(
-                "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         Matcher matcher = pattern.matcher(email);
 
         return matcher.find();
